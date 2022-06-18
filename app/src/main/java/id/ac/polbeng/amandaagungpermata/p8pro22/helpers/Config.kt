d21a21bd151f3f30cbfadecc82eb0bc1ffd754e3
Shirodevolop <emilioelva11@gmail.com>
2022-06-18 11:17:12 +0700

    crud mate

---
+++ app/src/main/java/id/ac/polbeng/amandaagungpermata/p8pro22/helpers/Config.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.amandaagungpermata.p8pro22.helpers
+
+class Config {
+    companion object {
+        const val SPLASH_TIME_OUT:Long = 3000
+        const val BASE_URL = "http://192.168.43.3:3000/"
+        const val EXTRA_FRAGMENT_ID = "FRAGMENT_ID"
+    }
+}
